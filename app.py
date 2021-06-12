5ecc0d0f824d5259029b65773012edf31660259c
Andrea_Graziosi <andreaggraziosi@gmail.com>
2021-06-12 13:36:04 -0700

    add secret key

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+
+#get access to our key-pair values from the .env file
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
 
 app = Flask(__name__)
 
